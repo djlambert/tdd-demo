8ac79d54f4f8d91f510bccf2039bc7cfe0641861
Derek Lambert <dlambert@dereklambert.com>
2021-10-20 21:09:09 -0500

    Add get_json method

--- src/demo_client/demo_client.py
+++ src/demo_client/demo_client.py
@@ -1,2 +1,10 @@
+from typing import Any, Dict, Text
+
+import requests
+
+
 class DemoClient:
-    ...
+    def get_json(self) -> Dict[Text, Any]:
+        response = requests.get('https://httpbin.org/json')
+
+        return response.json()
