83425d57d23d75e0ae2556d592a233ddc5eabac5
Derek Lambert <dlambert@dereklambert.com>
2021-10-21 09:43:45 -0500

    New feature

--- src/demo_client/demo_client.py
+++ src/demo_client/demo_client.py
@@ -1,2 +1,9 @@
+from typing import Any, Dict, Text
+
+import requests
+
 class DemoClient:
-    ...
+    def get_json(self) -> Dict[Text, Any]:
+        response = requests.get('https://httpbin.org/json')
+
+        return response.json()
