a172f138e95a2ca5a5968d040f4158a351f9b11b
Derek Lambert <dlambert@dereklambert.com>
2021-10-21 08:56:36 -0500

    Fix formatting

--- src/demo_client/demo_client.py
+++ src/demo_client/demo_client.py
@@ -3,9 +3,7 @@ from typing import Any, Dict, Text
 import requests
 
 
-
 class DemoClient:
-
 
     def get_json(self) -> Dict[Text, Any]:
         response = requests.get('https://httpbin.org/json')
